bdf6f1967b261d66994f5379c9a470b18aa6110b
unknown <habib.glai2019@gmail.com>
2023-01-26 11:20:42 +0100

    conversation v2

--- src/components/public-c/Conversation.js
+++ src/components/public-c/Conversation.js
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { db } from './firebase';
 
+//Conversation component yekhou conversationId as a prop w yekhdem bil useEffect hook bech ya3mel subscribe lil updates 
+//eli fil les messages mta3 conversation eli bech yekhouha mil ID eli fil Firebase database. 
+//component bech yraja3 les messages bil map function w ya3mel displays fi unordered list.
+//wa9teli el user submits el form, new message bech yetzed lil conversation's messages fil database.
+
 function Conversation({ conversationId }) {
   const [messages, setMessages] = useState([]);
 
